e39266ead6c9c7831f064be5036eaa41c44760d0
Iris Ng <irisngwl@gmail.com>
2023-03-19 16:16:56 +0800

    Temporarily resolve errors from client.py

--- value_of_serializability/client.py
+++ value_of_serializability/client.py
@@ -4,8 +4,8 @@ from random import randint
 from psycopg2 import extensions
 
 NUM_THREADS = 20
-NUM_OF_ROWS_IN_DATA = 1000
-id_counter = 0
+NUM_OF_ROWS_IN_DATA = 100000
+id_counter = 1
 END_FLAG = False
 CONSTANT_SUM = 500000500000
 NUM_OF_SWAP_TRANSACTION = 1000
@@ -17,9 +17,14 @@ LIBRARY_ISOLATION_LEVELS = {
   "REPEATABLE_READ": extensions.ISOLATION_LEVEL_REPEATABLE_READ,
   "SERIALIZABLE": extensions.ISOLATION_LEVEL_SERIALIZABLE,
 }
-ISOLATION_LEVEL = LIBRARY_ISOLATION_LEVELS["READ_COMMITTED"]
+ISOLATION_LEVEL = LIBRARY_ISOLATION_LEVELS["SERIALIZABLE"]
 
-def execute_sum_client():
+def execute_sum_client(results):
+  global END_FLAG
+  global ISOLATION_LEVEL
+  global CONSTANT_SUM
+  global end_flag_lock
+
   sum_count = 0
   sum_correct_count = 0
   while True:
@@ -32,9 +37,17 @@ def execute_sum_client():
     result = sum_b(ISOLATION_LEVEL)
     if result == CONSTANT_SUM:
       sum_correct_count += 1
-  return (sum_count, sum_correct_count)
+
+  results.append([sum_count, sum_correct_count])
     
 def execute_swap_client():
+  global NUM_OF_SWAP_TRANSACTION
+  global ISOLATION_LEVEL
+  global END_FLAG
+  global id_counter
+  global id_counter_lock
+  global end_flag_lock
+
   while True:
     id_counter_lock.acquire()
     if id_counter >= NUM_OF_SWAP_TRANSACTION * 2:
@@ -49,21 +62,33 @@ def execute_swap_client():
   end_flag_lock.release()
     
 def main():
+  global NUM_THREADS
+  
   # setup db
   setup_db()
+
   # execute the sum_thread
-  sum_thread = Thread(target=execute_sum_client)
-  # execute all the swap threads
-  # start timer
+  results = []
+  sum_thread = Thread(target=execute_sum_client, args=(results,))
+
   swap_threads = list()
+
+  # execute all the swap threads
   for i in range(NUM_THREADS):
     swap_thread = Thread(target=execute_swap_client)
     swap_threads.append(swap_thread)
+
+  # start timer
+  sum_thread.start()
+  for i in range(NUM_THREADS):
+    swap_threads[i].start()
   for i in range(NUM_THREADS):
     swap_threads[i].join()
   # end timer
   sum_thread.join()
-  (sum_count, sum_correct_count) = sum_thread.result
+  (sum_count, sum_correct_count) = results[0]
+  print('sum_count', sum_count)
+  print('sum_correct_count', sum_correct_count)
   
 if __name__ == '__main__':
     main()
