90aad31d5f2d51a60ff174e3b2406a745cee9949
Iris Ng <irisngwl@gmail.com>
2023-03-23 22:10:00 +0800

    Add separate docker-compose file for each experiment folder, remove docker-compose from root folder

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ## Setup
 
  1. Install Docker https://www.docker.com/products/docker-desktop/
- 2. Using terminal, cd to root folder (where docker-compose.yml is situated), run 'docker-compose up --build', this starts postgres container and runs the python script in another container
+ 2. Using terminal, cd to any experiment folder where respective experiment's docker-compose.yml is situated, run 'docker-compose up --build', this starts postgres container, and runs the python script in another container a few seconds after the database is up
  3. Any log output from python script will appear in the terminal
  4. If there is a need to rerun the python script, exit the terminal (using ctrl + c on Windows), this will exit both containers, then run 'docker-compose up' to restart both containers
 
---
+++ nonrepeatable_read/client.py
@@ -0,0 +1,44 @@
+from threading import Thread, Lock
+from psycopg2 import extensions
+
+
+# Must be a key from LIBRARY_ISOLATION_LEVELS
+ISOLATION_LEVEL_STRING = "READ_COMMITTED"
+# Must be a key from SELECT_QUERY_TYPES
+SELECT_QUERY_TYPE = "NORMAL_SELECT"
+
+SELECT_QUERY_TYPES = {
+   "NORMAL_SELECT": "",
+   "SELECT_FOR_UPDATE": " FOR UPDATE"
+}
+
+LIBRARY_ISOLATION_LEVELS = {
+  "READ_COMMITTED": extensions.ISOLATION_LEVEL_READ_COMMITTED,
+  "REPEATABLE_READ": extensions.ISOLATION_LEVEL_REPEATABLE_READ,
+  "SERIALIZABLE": extensions.ISOLATION_LEVEL_SERIALIZABLE,
+}
+ISOLATION_LEVEL = LIBRARY_ISOLATION_LEVELS[ISOLATION_LEVEL_STRING]
+
+NUM_THREADS = 10
+START_BALANCE = 10000
+WITHDRAWAL_AMOUNT = 10
+NUM_OF_TRANSACTION = START_BALANCE / WITHDRAWAL_AMOUNT * 1.1
+
+def print_experiment_settings():
+  print("------------ EXPERIMENT SETTINGS ------------")
+  print("Isolation Level: ", ISOLATION_LEVEL_STRING)
+  print("Number of Threads: ", NUM_THREADS)
+  print("Start Balance: ", START_BALANCE)
+  print("Withdrawal Amount: ", WITHDRAWAL_AMOUNT)
+  print("---------------------------------------------")
+
+
+    
+    
+
+def main():
+   return
+
+if __name__ == '__main__':
+    main()
+    
