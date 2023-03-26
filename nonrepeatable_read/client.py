eadaa260ee5480bd9ca677ebaab12d682dbd0fbe
Iris Ng <irisngwl@gmail.com>
2023-03-26 16:42:45 +0800

    Add stats printer, change flow of client.py to execute a set of experiments, and print setup and results after all experiments are executed

--- nonrepeatable_read/client.py
+++ nonrepeatable_read/client.py
@@ -1,42 +1,52 @@
 from threading import Thread, Lock
 from psycopg2 import extensions
-import time
 from db import *
+from stats import Statistics
 
 
-# Must be a key from LIBRARY_ISOLATION_LEVELS
-ISOLATION_LEVEL_STRING = "READ_COMMITTED"
-# Must be a key from SELECT_QUERY_TYPES
-SELECT_TYPE_STRING = "NORMAL_SELECT"
+EXPERIMENT_SETUP = [
+    {
+        # Must be a key from LIBRARY_ISOLATION_LEVELS
+        "ISOLATION_LEVEL": "READ_COMMITTED",
+        # Must be a key from SELECT_QUERY_TYPES
+        "SELECT_QUERY_TYPE": "NORMAL_SELECT",
+    },
+    {
+        "ISOLATION_LEVEL": "READ_COMMITTED",
+        "SELECT_QUERY_TYPE": "SELECT_FOR_UPDATE"
+    },
+    {
+        "ISOLATION_LEVEL": "REPEATABLE_READ",
+        "SELECT_QUERY_TYPE": "NORMAL_SELECT"
+    },
+    {
+        "ISOLATION_LEVEL": "SERIALIZABLE",
+        "SELECT_QUERY_TYPE": "NORMAL_SELECT"
+    },
+]
 
 SELECT_QUERY_TYPES = {
     "NORMAL_SELECT": "",
     "SELECT_FOR_UPDATE": " FOR UPDATE"
 }
-SELECT_QUERY_TYPE = SELECT_QUERY_TYPES[SELECT_TYPE_STRING]
+select_query_type = None
 
 LIBRARY_ISOLATION_LEVELS = {
     "READ_COMMITTED": extensions.ISOLATION_LEVEL_READ_COMMITTED,
     "REPEATABLE_READ": extensions.ISOLATION_LEVEL_REPEATABLE_READ,
     "SERIALIZABLE": extensions.ISOLATION_LEVEL_SERIALIZABLE,
 }
-ISOLATION_LEVEL = LIBRARY_ISOLATION_LEVELS[ISOLATION_LEVEL_STRING]
+isolation_level = None
 
 NUM_THREADS = 10
 START_BALANCE = 200000
 WITHDRAWAL_AMOUNT = 20
-NUM_OF_TRANSACTION = START_BALANCE / WITHDRAWAL_AMOUNT * 1.1
+# This should be kept constant across the different experiments
+# and should cause transactions to continue executing even after balance reach 0
+# to check if the experiment setup will result in overdeduction of balance due to concurrency issue
+NUM_OF_TRANSACTION = START_BALANCE / WITHDRAWAL_AMOUNT * 1.1 
 transaction_counter = 0
 transaction_counter_lock = Lock()
-
-
-def print_experiment_settings():
-    print("------------ EXPERIMENT SETTINGS ------------")
-    print("Isolation Level: ", ISOLATION_LEVEL_STRING)
-    print("Number of Threads: ", NUM_THREADS)
-    print("Start Balance: ", START_BALANCE)
-    print("Withdrawal Amount: ", WITHDRAWAL_AMOUNT)
-    print("---------------------------------------------")
 
 
 def execute_withdrawal_client():
@@ -51,41 +61,65 @@ def execute_withdrawal_client():
         transaction_counter += 1
         transaction_counter_lock.release()
         withdrawal_transaction(
-            ISOLATION_LEVEL, SELECT_QUERY_TYPE, WITHDRAWAL_AMOUNT)
+            isolation_level, select_query_type, WITHDRAWAL_AMOUNT)
+
+
+
+def reset_global_parameters(experiment_parameters):
+    global transaction_counter
+    global isolation_level
+    global select_query_type
+    
+    transaction_counter = 0
+    isolation_level = LIBRARY_ISOLATION_LEVELS[experiment_parameters["ISOLATION_LEVEL"]]
+    select_query_type = SELECT_QUERY_TYPES[experiment_parameters["SELECT_QUERY_TYPE"]]
+
+
+def run_experiments():
+    experiment_stats = []
+
+    print('Running all experiments... Please Wait...')
+    for experiment_parameters in EXPERIMENT_SETUP:
+        setup_db()
+        stats = Statistics()
+        stats.set_experiment_parameters({**experiment_parameters, "NUM_THREADS": NUM_THREADS, "START_BALANCE": START_BALANCE, "WITHDRAWAL_AMOUNT": WITHDRAWAL_AMOUNT})
+        reset_global_parameters(experiment_parameters)
+        reset_balance(START_BALANCE)
+
+        withdrawal_threads = list()
+        for i in range(NUM_THREADS):
+            withdrawal_thread = Thread(target=execute_withdrawal_client)
+            withdrawal_threads.append(withdrawal_thread)
+
+        stats.start_timer()
+
+        # Concurrently execute transactions
+        for i in range(NUM_THREADS):
+            withdrawal_threads[i].start()
+
+        for i in range(NUM_THREADS):
+            withdrawal_threads[i].join()
+
+        stats.end_timer()
+
+        # Get end balance to check correctness
+        stats.set_end_balance(find_end_balance())
+
+        experiment_stats.append(stats)
+        print('Finished an experiment...')
+
+    return experiment_stats
+
+def print_experiment_stats(experiment_stats):
+    for stats in experiment_stats:
+        stats.print_experiment_setup()
+        stats.print_results()
 
 
 def main():
-    setup_db()
-    print_experiment_settings()
+    experiment_stats = run_experiments()
+    print_experiment_stats(experiment_stats)
 
-    reset_balance(START_BALANCE)
-
-    withdrawal_threads = list()
-
-    for i in range(NUM_THREADS):
-        withdrawal_thread = Thread(target=execute_withdrawal_client)
-        withdrawal_threads.append(withdrawal_thread)
-
-    # start timer
-    start_time = time.perf_counter()
-    for i in range(NUM_THREADS):
-        withdrawal_threads[i].start()
-
-    for i in range(NUM_THREADS):
-        withdrawal_threads[i].join()
-
-    # check correctness
-    end_balance = find_end_balance()
-    print('End balance: ', end_balance)
-    print('\nBased on application level check implemented during withdrawal transaction,\nwithdrawal (UPDATE statement) should only be executed if there is sufficient prior balance (from SELECT statement).\nHence, end balance ideally should be >= 0 (not negative) if no concurrency anomaly is encountered.')
-    print('Is end balance valid? ', end_balance >= 0)
-
-    # end timer
-    end_time = time.perf_counter()
-
-    # calculate throughput
-    response_time = end_time - start_time
-    print('response time in seconds', response_time)
 
 
 if __name__ == '__main__':
